f8ce9ca49d95103bbb41fc31ca522d5f1e8d6177
markdmatthews <mark@thematthews.org>
2020-03-04 14:46:29 -0600

    Adding a very simple example OpMode

---
+++ TeamCode/src/main/java/com/ftc9929/examples/markdmatthews/SimpleTeleOp.java
@@ -0,0 +1,54 @@
+/*
+ Copyright (c) 2020 The Tech Ninja Team (https://ftc9929.com)
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+package com.ftc9929.examples.markdmatthews;
+
+import com.ftc9929.corelib.control.NinjaGamePad;
+import com.ftc9929.corelib.control.ToggledButton;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * A very simple OpMode that changes what is sent to the drivers' station
+ * telemetry each time the driver "a" button is pressed.
+ */
+@TeleOp(name="Mark's Example")
+public class SimpleTeleOp extends OpMode {
+    private NinjaGamePad gamepad;
+
+    private ToggledButton aButtonToggled;
+
+    @Override
+    public void init() {
+        gamepad = new NinjaGamePad(gamepad1);
+        aButtonToggled = new ToggledButton(gamepad.getAButton());
+    }
+
+    @Override
+    public void loop() {
+        if (aButtonToggled.isToggledTrue()) {
+            telemetry.addData("toggle", "ping");
+        } else {
+            telemetry.addData("toggle", "pong");
+        }
+
+        updateTelemetry(telemetry);
+    }
+}
